e7a1007a29e40b778f0cb065719d5514fb333237
schleising <stephen@schleising.net>
2021-06-01 22:23:44 +0100

    Add some tests

--- go/struct-test.go
+++ go/struct-test.go
@@ -11,8 +11,28 @@ type Record struct {
 func main() {
 	input := []string{"four", "five", "six"}
 	record1 := Record{"1", "2.3", "Test"}
-	record2 := Record{one: input[0], two: input[1], three: input[2]}
+	record2 := Record{
+		one:   input[0],
+		three: input[1],
+		two:   input[2],
+	}
 
 	fmt.Println(record1)
 	fmt.Println(record2)
+
+	map1 := map[string]int{}
+
+	fmt.Println(map1)
+
+	map2 := map[string]int{
+		"Three": 3,
+		"Four":  4,
+		"One":   5,
+	}
+
+	for k, v := range map2 {
+		map1[k] = v
+	}
+
+	fmt.Println(map1)
 }
